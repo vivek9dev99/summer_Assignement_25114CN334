c8f8598b60c3ec5eab0685bb327bf40da998dec3
vivek9dev99 <vivek0905yadav@gmail.com>
2026-06-11 18:18:05 +0530

    Add files via upload

---
+++ que3.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+
+int main() {
+    int num;
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+    if (num <= 0) {
+ printf("Please enter a valid positive integer.\n");
+ return 1;
+    }
+    printf("Factors of %d are: ", num);
+    for (int i = 1; i <= num; i++) {
+    if (num % i == 0) {
+    printf("%d ", i);
+       }
+    }
+    printf("\n");
+    return 0;
+}
