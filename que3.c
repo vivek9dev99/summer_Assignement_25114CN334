1837ce8ef47a739640e61617ba474d8d8a583461
vivek9dev99 <vivek0905yadav@gmail.com>
2026-06-11 18:09:03 +0530

    Add files via upload

---
+++ que3.c
@@ -0,0 +1,11 @@
+//Write a program to Find GCD of two numbers. 
+#include<stdio.h>
+int main()
+{int a,b;
+printf("enter the number:");
+scanf("%d",&a);
+printf("enter the number:");
+scanf("%d",&b);
+a>b?printf("a is greater"):printf("b is greater");
+return 0;
+}
