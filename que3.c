67b7103a8f805d59ee2f9e4f7432bcac706a702c
vivek9dev99 <vivek0905yadav@gmail.com>
2026-06-11 18:36:04 +0530

    Add files via upload

---
+++ que3.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+int main() {
+    int i, j;
+    int rows = 5; 
+    for (i = 1; i <= rows; i++) {
+    
+        for (j = 1; j <= i; j++) {
+            printf("%c", 'A' + (i - 1));
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
