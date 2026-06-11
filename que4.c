b4fb9496a28deaa9befe5817861348f67d85fda1
vivek9dev99 <vivek0905yadav@gmail.com>
2026-06-11 18:31:01 +0530

    Add files via upload

---
+++ que4.c
@@ -0,0 +1,15 @@
+//wap to print repeated number pattern
+#include <stdio.h>
+
+int main() {
+    int i, j, rows = 5;
+
+    for(i = 1; i <= rows; i++) {
+        for(j = 1; j <= i; j++) {
+            printf("%d", i);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
