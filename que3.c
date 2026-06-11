fbad355e3072f942c98dcff64c9dde74b9600735
vivek9dev99 <vivek0905yadav@gmail.com>
2026-06-11 18:57:01 +0530

    Add files via upload

---
+++ que3.c
@@ -0,0 +1,32 @@
+//wap to write function to check prime
+#include <stdio.h>
+int isPrime(int n) {
+    if (n <= 1) {
+        return 0;
+    }
+    if (n == 2) {
+        return 1;
+    }
+    if (n % 2 == 0) {
+        return 0;
+    }
+    for (int i = 3; i * i <= n; i += 2) {
+        if (n % i == 0) {
+            return 0;
+        }
+    }
+    return 1; 
+}
+int main() {
+    int num;
+    
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+    if (isPrime(num)) {
+        printf("%d is a prime number.\n", num);
+    } else {
+        printf("%d is not a prime number.\n", num);
+    }
+    return 0;
+}
+
